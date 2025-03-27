ec36b8ddbba4b266682a711db1926ef198cb3ad6
akainu00s <markandradeyu@gmail.com>
2025-03-27 18:23:59 +0800

    Update tools.js

--- src/tools.js
+++ src/tools.js
@@ -58,7 +58,7 @@ const tools = {
     // "info": {
     //     icon: fa_info_circle,
     //     callback: () => {
-    //         open("https://github.com/stevenjoezhang/live2d-widget");
+    //         open("https://github.com/marklacanaria/wgt");
     //     }
     // },
     // "quit": {
